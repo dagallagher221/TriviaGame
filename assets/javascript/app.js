297e9a90f35baaf5479df9d9ffaf8df8c91d88f1
Dan Gallagher <dagallagher221@gmail.com>
2019-09-22 14:51:26 -0400

    got game to function correctly, began adding placeholder gifs and questions and basic css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,16 +6,53 @@ let loss = 0;
 let timer;
 
 
+// If the timer is over, then go to the next question
+function nextQuestion() {
+    const isQuestionOver = (quizQuestions.length - 1) === currentQuestion;
+    if (isQuestionOver) {
+        console.log("Game is Over");
+        displayResult();
+    }
+    else {
+        currentQuestion++;
+        loadQuestion();
+    }
+}
+
+
+
+//Start a 30 second timer for user to respond or choose an answer to each question
+
+function timeUp() {
+    clearInterval(timer);
+    loss++;
+    preloadImage('lost');
+    setTimeout(nextQuestion, 3 * 1000);
+}
+
+function countDown() {
+    counter--;
+    $('#time').html('Timer : ' + counter);
+    if (counter === 0) {
+        timeUp();
+    }
+}
+
 //Display the question and the choices in the browser
 
 function loadQuestion() {
+    counter = 30;
+    timer = setInterval(countDown, 1000);
+
+
     const question = quizQuestions[currentQuestion].question; //pulls the question attribute from the quiz question object
     const choices = quizQuestions[currentQuestion].choices;  //pulls the choices attribute
     $('#time').html("Time Left: " + counter);
-    
+
     $("#game").html(`
     <h4>${question}</h4>
-    ${loadChoices(choices)};
+    ${loadChoices(choices)}
+    ${loadRemainingQuestion()}
     `);
 
 }
@@ -23,10 +60,88 @@ function loadQuestion() {
 function loadChoices(choices) {
     let result = '';
 
-    for (let i=0; i < choices.length; i++) {
+    for (let i = 0; i < choices.length; i++) {
         result += `<p class = "choice" data-answer="${choices[i]}">${choices[i]}</p>`;
     }
     return result;
 }
 
-loadQuestion();
+$(document).on('click', '.choice', function () {
+    clearInterval(timer);
+    const selectedAnswer = $(this).attr('data-answer');
+    const correctAnswer = quizQuestions[currentQuestion].correctAnswer;
+
+    if (selectedAnswer === correctAnswer) {
+        score++;
+        console.log('Winssss!!!');
+        preloadImage('win');
+        setTimeout(nextQuestion, 3 * 1000);
+    } else {
+        loss++;
+        console.log("Lost!!!");
+        preloadImage('lost');
+        setTimeout(nextQuestion, 3 * 1000);
+    }
+})
+
+function displayResult() {
+    const result = `
+    <p>You get ${score} question(s) right!</p>
+    <p>You missed ${loss} question(s)</p>
+    <p>Total Quesions ${quizQuestions.length} question(s) right</p>
+    <button class="btn btn-primary" id="reset">Reset Game</button>
+    `;
+
+    $("#game").html(result);
+}
+
+$(document).on('click', '#reset', function () {
+    counter = 30;
+    currentQuestion = 0;
+    score = 0;
+    loss = 0;
+    timer = null;
+
+    loadQuestion();
+})
+
+function loadRemainingQuestion() {
+    const remainingQuestion = quizQuestions.length - (currentQuestion + 1);
+    const totalQuestion = quizQuestions.length;
+    return `Remaining Question: ${remainingQuestion}/${totalQuestion}`;
+
+
+};
+
+function randomImage(images) {
+    const random = Math.floor(Math.random() * images.length);
+    const randomImage = images[random];
+    return randomImage;
+}
+
+function preloadImage(status) {
+    const correctAnswer = quizQuestions[currentQuestion].correctAnswer;
+
+    if (status === 'win') {
+        $('#game').html(`
+        <p class="preload-image">Congratulations, you picked the correct answer</p>
+        <p class="preload-image">The correct answer is <b>${correctAnswer}</b></p>
+        <img src="${randomImage(funImages)}" />
+        `);
+    }
+    else {
+        $('#game').html(`
+            <p class="preload-image">The correct answer was <b>${correctAnswer}</b></p>
+            <p class="preload-image">You lost</p>
+            <img src="${randomImage(sadImages)}" />
+            `);
+    }
+
+}
+
+
+$("#start").click(function () {
+    $("#start").remove();
+    $("#time").html(counter);
+    loadQuestion();
+});
--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,13 +1,13 @@
 const quizQuestions = [
     {
-        question: 'What is the answer to this question?',
-        choices: ["This one", "Not this one", "Not this one", "Not this one"],
-        correctAnswer: "This one"
+        question: "Who's statue lies before Blackrock Mountain?",
+        choices: ["Nathanos Blightcaller", "Orgrim Doomhammer", "Anduin Lothar", "Varian Wrynn"],
+        correctAnswer: "Anduin Lothar"
     },
     {
-        question: 'What is the answer to this question?',
-        choices: ["This one", "Not this one", "Not this one", "Not this one"],
-        correctAnswer: "This one"
+        question: 'Which of the following is not an Old God?',
+        choices: ["C'thun", "Anub'arak", "Yogg'saron", "N'zoth"],
+        correctAnswer: "Anub'arak"
     },
     {
         question: 'What is the answer to this question?',
@@ -41,3 +41,17 @@ const quizQuestions = [
     },
 
 ]
+
+const funImages = [
+    './assets/images/happy fun.gif',
+    './assets/images/hellYeah.gif',
+    './assets/images/winning.gif',
+    './assets/images/yeah.gif',
+    './assets/images/you did it.gif'
+];
+
+const sadImages = [
+    './assets/images/danm.gif',
+    './assets/images/oops.gif',
+    './assets/images/pretty bad.gif'
+];
