129dae5de7ac92a1ea346ebecb78e4adebc641fe
Dan Gallagher <dagallagher221@gmail.com>
2019-09-22 12:02:01 -0400

    added basic structure to html and a second js file to hold questions

---
+++ assets/javascript/questions.js
@@ -0,0 +1,37 @@
+const quizQuestions = [
+    {
+        question: '',
+        choices: [1, 2, 3, 4],
+        correctAnswer: 4
+    },
+    {
+        question: '',
+        choices: [1, 2, 3, 4],
+        correctAnswer: 2
+    },
+    {
+        question: '',
+        choices: [1, 2, 3, 4],
+        correctAnswer: 1
+    },
+    {
+        question: '',
+        choices: [1, 2, 3, 4],
+        correctAnswer: 3
+    },
+    {
+        question: '',
+        choices: [1, 2, 3, 4],
+        correctAnswer: 1
+    },
+    {
+        question: '',
+        choices: [1, 2, 3, 4],
+        correctAnswer: 4
+    },
+    {
+        question: '',
+        choices: [1, 2, 3, 4],
+        correctAnswer: 2
+    }
+]
